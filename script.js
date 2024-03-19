25cc090777194b67de08c7e14673a92a205d7f12
KABOOTCHER <you@example.com>
2024-03-19 17:18:21 +0300

    adaptive

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if (window.location.protocol === "file:") {
+
+    document.body.innerHTML = "<h1> Вы не можете открыть файл локально. </h1>";
+}
+
+
+
+
+
+
 const body = document.querySelector('html')
 const initialTheme = "light"
 
@@ -21,3 +31,19 @@ const setThemeOnInit = () =>{
 }
 
 setThemeOnInit()
+
+
+const toggleNavbar = () => {
+    let sidebar = document.getElementById("sidebar");
+    let navbarToggler = document.querySelector(".navbarToggler");
+    sidebar.classList.toggle("hidden")
+
+    if(sidebar.classList.contains("hidden" )== false){
+        navbarToggler.style.backgroundImage = 'url(img/close.png)';
+    } else{
+        navbarToggler.style.backgroundImage = 'url(img/hamburger.png)';
+    }
+};
+
+
+
