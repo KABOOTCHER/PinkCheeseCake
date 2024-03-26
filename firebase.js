9cf0eee825ec5e0361a20eaabce80742556aebec
KABOOTCHER <you@example.com>
2024-03-26 13:28:40 +0300

    added firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAtw6tJb4I-kIZH4yipRSHXYBv0aZODvBA",
+    authDomain: "pink-cheese-cake.firebaseapp.com",
+    projectId: "pink-cheese-cake",
+    storageBucket: "pink-cheese-cake.appspot.com",
+    messagingSenderId: "501778632124",
+    appId: "1:501778632124:web:ed0b1f124f3f5bc6032fb9"
+  };
